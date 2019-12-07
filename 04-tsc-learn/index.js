40a025be58f8efc9857465e59a6a14c110241a13
lhw8420 <1980370947@qq.com>
2019-12-08 00:37:33 +0800

    类型一旦定义好了之后就不能再修改为其他类型

---
+++ 04-tsc-learn/index.js
@@ -0,0 +1,3 @@
+var my_name = 'rail1235';
+var my_name = 123;
+console.log('这样会报错,myname类型不能重新修改');
---
+++ 04-tsc-learn/index.ts
@@ -0,0 +1,4 @@
+var my_name: string = 'rail1235'
+var my_name: number = 123
+
+console.log('这样会报错,myname类型不能重新修改')
