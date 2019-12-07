a249941f49c5d3af80a9409194d4a1bf54f4504a
lhw8420 <1980370947@qq.com>
2019-12-08 00:14:51 +0800

    字符串、布尔值、数组、对象、函数例子

--- 03-tsc-learn/index.js
+++ 03-tsc-learn/index.js
@@ -1,1 +1,17 @@
-var str = 'hello git, hello typeScript';
+var str;
+str = '10';
+var bool;
+bool = true;
+var num;
+num = 10;
+var arr;
+arr = [1, 2];
+var obj;
+obj = {
+    'a': 1,
+    'b': 2
+};
+function add(a, b) {
+    return a + b;
+}
+add(1, 6);
--- 03-tsc-learn/index.ts
+++ 03-tsc-learn/index.ts
@@ -1,1 +1,22 @@
-const str = 'hello git, hello typeScript';
+var str: string;
+str = '10';
+
+var bool: boolean;
+bool = true
+
+var num: number;
+num=10
+
+var arr: object;
+arr=[1,2]
+
+var obj: object
+obj = {
+    'a':1,
+    'b':2
+}
+
+function add(a:number, b:number): number{
+    return a +b                               
+}
+add(1,6)
