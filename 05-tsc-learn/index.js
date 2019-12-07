4aaf3a391d24f56926310759ed8c5643bbf4a03f
lhw8420 <1980370947@qq.com>
2019-12-08 01:01:20 +0800

    1、数组里的元素必须为字符串写法2、一个数据类型定义过之后，赋值给另一个字段的时候不需要在定义

--- 05-tsc-learn/index.js
+++ 05-tsc-learn/index.js
@@ -1,7 +1,6 @@
 // 第一种写法：数组里的元素必须为字符串
-let myArr:string[] = ["123", ""]
+var myArr = ["123", ""];
 // 第二种写法：
-let arr: Array<number> = [1,2,3]
-
+var arr = [1, 2, 3];
 //一个数据类型定义过之后，赋值给另一个字段的时候不需要在定义
-let arr2 = arr
+var arr2 = arr;
