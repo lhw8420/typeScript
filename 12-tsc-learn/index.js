e8f9e68abc5f43a6902310f301e83c1f20712dca
lhw8420 <1980370947@qq.com>
2019-12-10 21:57:02 +0800

    补充isString isNumber函数，return true没效果

--- 12-tsc-learn/index.js
+++ 12-tsc-learn/index.js
@@ -5,14 +5,22 @@ a = [1, 2];
 a = {
     'a': 1
 };
+// 判断是一个数字
+function isNumber(value) {
+    return typeof value === 'number';
+}
+// 判断是一个字符串
+function isString(value) {
+    return typeof value === 'string';
+}
 var log = function (value) {
-    if (typeof value === 'number')
-        return "your value is " + value;
-    if (typeof value === 'string')
-        return "your value is " + value;
+    if (isNumber(value))
+        return "your value is number " + value;
+    if (isString(value))
+        return "your value is string " + value;
     throw new Error("Expected string or number, got " + value);
 };
-console.log(log([1, 2]));
+console.log(log(2));
 // 数组中的元素是可任意的
 var b;
 b = [1, 2, '3', []];
--- 12-tsc-learn/index.ts
+++ 12-tsc-learn/index.ts
@@ -7,15 +7,25 @@ a = {
     'a':1
 }
 
+// 判断是一个数字
+function isNumber(value: any): value is number {
+    return typeof value === 'number';
+}
+
+// 判断是一个字符串
+function isString(value: any): value is string {
+    return typeof value === 'string';
+}
+
 const log = (value: any) => {
-    if(typeof value === 'number')
-        return `your value is ${value}`;
-    if(typeof value === 'string')
-        return `your value is ${value}`;
+    if(isNumber(value))
+        return `your value is number ${value}`;
+    if(isString(value))
+        return `your value is string ${value}`;
     throw new Error(`Expected string or number, got ${value}`)
 }
 
-console.log(log([1,2]))
+console.log(log(2))
 
 // 数组中的元素是可任意的
 let b: any[];
